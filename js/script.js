11c8b6295c79110e7cdb169f3f64df6482a97e28
DagemBefikadu <befikadu.dagem@gmail.com>
2021-10-27 20:03:24 -0700

    Win and loser text shows

--- js/script.js
+++ js/script.js
@@ -26,22 +26,22 @@ function spawnObjects(color, width, height) {
   this.color = color;
   this.width = width;
   this.height = height;
-  this.alive = true ;
-    if(this.color == 'brown') {
-        this.points = -1
-    }else if(this.color == 'blue') {
-        this.points = -1
-    } else if(this.color == 'red') {
-        this.points = 2
-    }else {
-        this.points = 1
-    }
+  this.alive = true;
+  if (this.color == "brown") {
+    this.points = -1;
+  } else if (this.color == "blue") {
+    this.points = -1;
+  } else if (this.color == "red") {
+    this.points = 2;
+  } else {
+    this.points = 1;
+  }
 
   this.render = function () {
-      if(this.alive === true) {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-      }
+    if (this.alive === true) {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
   };
 }
 
@@ -53,25 +53,27 @@ console.log("plyer at", player);
 const gameLoop = () => {
   ctx.clearRect(0, 0, game.width, game.height);
   player.render();
-   for ( let i = 0; i < courtObjects.length; i++) {
-       courtObjects[i].render()
-       if(player.x < courtObjects[i].x + courtObjects[i].width && 
-        player.x + player.width > courtObjects[i].x &&
-        player.y < courtObjects[i].y + courtObjects[i].height &&
-        player.y + player.height > courtObjects[i].y && courtObjects[i].alive == true ){
-            console.log("hi")
-        courtObjects[i].alive = false
-        courtObjects.splice(courtObjects[i],0)
-        pointCounter+= courtObjects[i].points;
-        points.innerHTML = pointCounter
-        }
- 
+  for (let i = 0; i < courtObjects.length; i++) {
+    courtObjects[i].render();
+    if (
+      player.x < courtObjects[i].x + courtObjects[i].width &&
+      player.x + player.width > courtObjects[i].x &&
+      player.y < courtObjects[i].y + courtObjects[i].height &&
+      player.y + player.height > courtObjects[i].y &&
+      courtObjects[i].alive == true
+    ) {
+      console.log("hi");
+      courtObjects[i].alive = false;
+      courtObjects.splice(courtObjects[i], 0);
+      pointCounter += courtObjects[i].points;
+      points.innerHTML = pointCounter;
     }
-}
+  }
+};
 //With this empty array I can push in all my objects, so I can use it later .(its global)
 let courtObjects = [];
 
-//This function gives me the ability to limit how many objects appear at a time, while stopping to many from appearing. 
+//This function gives me the ability to limit how many objects appear at a time, while stopping to many from appearing.
 function courtObject() {
   spawnInterval = setInterval(() => {
     let basketball = new spawnObjects("orange", 15, 15);
@@ -95,29 +97,40 @@ const movementHandler = (e) => {
     // player UP
     case 87:
       player.y -= 10;
+      if (player.y <= 0) {
+        player.y = 0;
+      }
       break;
     // left
     case 65:
       player.x -= 10;
+      if (player.x <= 0) {
+        player.x = 0;
+      }
       break;
     // down
     case 83:
       player.y += 10;
+      if (player.y + player.height >= game.height) {
+        player.y = game.height - player.height;
+      }
       break;
     // right
     case 68:
       player.x += 10;
+      if (player.x + player.width >= game.width) {
+        player.x = game.width - player.width;
+      }
       break;
   }
 };
 
 document.addEventListener("keydown", movementHandler);
 
-
 //Button is currently shot clock for now, we want to change that function to happen once the player moves.
 document.addEventListener("click", start);
 let time;
-let timeStart = 24;
+let timeStart = 5;
 
 const shotDoneClock = () => {
   clearInterval(time);
@@ -130,16 +143,32 @@ function shotClock() {
     // console.log(timeStart);
   } else {
     shotDoneClock();
+    stopGameLoop();
+    winnerAlert();
+  }
+}
+
+function winnerAlert() {
+  let myAlert = document.createElement("collected");
+  myAlert.setAttribute("role", "alert");
+  if (pointCounter > 16) {
+    let myAlertText = document.createTextNode("YOU WIN!!!!!!");
+    myAlert.appendChild(myAlertText);
+    document.body.appendChild(myAlert);
+  } else {
+    let myAlertText = document.createTextNode("TRY AGAIN");
+    myAlert.appendChild(myAlertText);
+    document.body.appendChild(myAlert);
   }
 }
 
 function start() {
-  time = setInterval(shotClock, 1000);
+  time = setInterval(shotClock, 2000);
 
   shotClock();
 }
 
-let stop = () => {
+let stopGameLoop = () => {
   clearInterval(gameInterval);
 };
 
